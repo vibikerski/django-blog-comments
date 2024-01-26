b43a5bbc3c4b35ad85e927d25067666d43b70ab0
vibikerski <vibikerski@gmail.com>
2024-01-26 19:10:17 +0100

    Models, migrations

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    date_creation = models.DateField()
+
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    date_creation = models.DateField()
+
+    under_post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.text
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 import django_setup
+from blogs.models import User, Post, Comment
